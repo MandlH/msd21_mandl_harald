7f02c77cacb39fa4b9869351e5d4bc39f2ef62f2
Harald Mandl <harald.mandl@edu.fh-joanneum.at>
2022-05-21 09:27:18 +0200

    updated pom.xml and testing log4j

--- Calculator/src/main/java/at/fhj/msd/Application.java
+++ Calculator/src/main/java/at/fhj/msd/Application.java
@@ -1,15 +1,24 @@
 package at.fhj.msd;
 
+import at.fhj.msd.Calculator;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
 public class Application {
 
+    private static Logger logger = LogManager.getLogger();
 
     public static void main(String[] args) {
+
         Calculator calculator = new Calculator();
-        System.out.println(calculator.add(3, 10));
+        System.out.println(calculator.add(5, 10));
         System.out.println(calculator.minus(21, 2));
         System.out.println(calculator.multiply(-1, 0));
         System.out.println(calculator.divide(2, 0));
         System.out.println(calculator.faculty(4));
+
+        logger.info("It is a info logger.");
+        logger.error("It is an error logger.");
 
     }
 }
---
+++ exercise5.md
@@ -0,0 +1,7 @@
+# Exercise5
+
+## Log4J Test
+ 
+### Info And Error
+
+Only error and fatal logs show up in the console.
